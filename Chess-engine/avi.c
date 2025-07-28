4e7a7d007f6271107913a6dda6b83dc3c7cd5d58
Arya Gupta <aryagupta164@gmail.com>
2025-07-29 01:58:35 +0530

    Succesfully got board array printed

--- Chess-engine/avi.c
+++ Chess-engine/avi.c
@@ -1,7 +1,18 @@
 # include "stdio.h"
 #include "defs.h"
-#include "init.c"
 int main(){
     AllInit();
+
+    for(int i = 0; i < BRD_SQ_NUM; i++){
+        if(i%10 == 0) printf("\n");
+        printf("%5d", Sq120ToSq64[i]);
+    }
+    printf("\n");
+    printf("\n");
+
+    for(int i = 0; i < 64; i++){
+        if (i%8==0) printf("\n");
+        printf("%5d", Sq64ToSq120[i]);
+    }
     return 0;
 }
---
+++ Chess-engine/tempCodeRunnerFile.c
@@ -0,0 +1,6 @@
+for(int i = 0; i < BRD_SQ_NUM; i++){
+        if(i%10 == 0)
+            printf("%5d", Sq120ToSq64[i]);
+    }
+    printf("\n");
+    printf("\n");
