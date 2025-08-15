e6b128ccb114b3216474f4146c3b92b81c348e37
Arya Gupta <aryagupta164@gmail.com>
2025-08-15 23:11:09 +0530

    Initialized project

---
+++ Black-Hole/black_hole.cpp
@@ -0,0 +1,21 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <vector>
+#include <iostream>
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include <sstream>
+#include <iomanip>
+#include <cstring>
+#include <chrono>
+#include <fstream>
+#include <sstream>
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+using namespace glm;
+using namespace std;
+using Clock = std::chrono::high_resolution_clock;
